b1ee714df74821719d094854cf8d7c6cb5b3ed7b
tebiev <19126494@>
2023-04-28 03:01:26 +0300

    d

--- src/main/scala/com/example/testweb/TestwebRoutes.scala
+++ src/main/scala/com/example/testweb/TestwebRoutes.scala
@@ -1,17 +1,19 @@
 package com.example.testweb
 
-import cats.effect.Sync
+import cats.effect.Async
 import cats.effect.std.Console
 import cats.implicits._
+import doobie.Transactor
+import doobie.implicits.toSqlInterpolator
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
 import java.nio.charset.StandardCharsets
-import scala.sys.process.Process
+import doobie.implicits._
 
 object TestwebRoutes {
 
-  def jokeRoutes[F[_]: Sync: Console](J: Jokes[F]): HttpRoutes[F] = {
+  def jokeRoutes[F[_]: Async: Console](J: Jokes[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}
     import dsl._
     HttpRoutes.of[F] {
@@ -20,9 +22,14 @@ object TestwebRoutes {
           reqBody <- req.body.compile.fold(Array.empty[Byte])(_ appended _)
             .map(new String(_, StandardCharsets.UTF_8))
           _ <- Console[F].println(reqBody)
-          _ <- Sync[F].delay(Process("curl 0.0.0.0:5432").run())
-          joke <- J.get
-          resp <- Ok(joke)
+
+          xa = Transactor.fromDriverManager[F](
+            "org.postgresql.Driver", "jdbc:postgresql:postgres", "postgres", "Stt-6789"
+          )
+
+          x <- sql"select * from test".query[Int].option.transact(xa).map(_.toString)
+          _ <- Console[F].println(x)
+          resp <- Ok(x)
         } yield resp
       case e => Ok(e.uri.toString)
     }
